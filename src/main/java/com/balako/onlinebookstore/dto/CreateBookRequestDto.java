69fa47091dbe3aabb0c2e77b93015eaee079f1b3
Denis Balako <denisarius2000s@gmail.com>
2023-08-31 23:25:28 +0300

    added CustomGlobalExceptionHandler and meaningful validation for DTO classes

--- src/main/java/com/balako/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/balako/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,23 @@
 package com.balako.onlinebookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
+    @Size(min = 1, max = 255)
     private String title;
+    @NotNull
+    @Size(min = 1, max = 255)
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(value = 0)
     private BigDecimal price;
     private String description;
     private String coverImage;
