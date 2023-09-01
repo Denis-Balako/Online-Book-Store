631d827edb1fc926e65388ecc36b117acc383334
Denis Balako <denisarius2000s@gmail.com>
2023-09-01 17:11:32 +0300

    added pagination and sorting

--- src/main/java/com/balako/onlinebookstore/service/BookService.java
+++ src/main/java/com/balako/onlinebookstore/service/BookService.java
@@ -4,11 +4,12 @@ import com.balako.onlinebookstore.dto.BookDto;
 import com.balako.onlinebookstore.dto.BookSearchParametersDto;
 import com.balako.onlinebookstore.dto.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto getById(Long id);
 
