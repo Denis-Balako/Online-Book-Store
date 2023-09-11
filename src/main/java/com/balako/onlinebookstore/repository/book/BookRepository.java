71fdf788e16cb95c68b57184b734f1dd888a6f2d
Denis Balako <denisarius2000s@gmail.com>
2023-09-11 21:54:45 +0300

    Added Auth controller doc in OpenApi and fixed checkstyle mistakes

--- src/main/java/com/balako/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/com/balako/onlinebookstore/repository/book/BookRepository.java
@@ -3,14 +3,12 @@ package com.balako.onlinebookstore.repository.book;
 import com.balako.onlinebookstore.model.Book;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.lang.NonNull;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
     @Query("SELECT DISTINCT b FROM Book b "
