910dd52229b4b8927ae61be841b4139cadc37ff2
Denis Balako <denisarius2000s@gmail.com>
2023-09-11 15:10:10 +0300

    added category mapper implementation

--- src/main/java/com/balako/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/com/balako/onlinebookstore/mapper/BookMapper.java
@@ -2,10 +2,15 @@ package com.balako.onlinebookstore.mapper;
 
 import com.balako.onlinebookstore.config.MapperConfig;
 import com.balako.onlinebookstore.dto.book.BookDto;
+import com.balako.onlinebookstore.dto.book.BookDtoWithoutCategoryIds;
 import com.balako.onlinebookstore.dto.book.CreateBookRequestDto;
 import com.balako.onlinebookstore.model.Book;
+import java.util.HashSet;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
@@ -13,4 +18,17 @@ public interface BookMapper {
 
     @Mapping(target = "id", ignore = true)
     Book toModel(CreateBookRequestDto requestDto);
+
+    BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
+
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookDto bookDto, Book book) {
+        bookDto.setCategories(new HashSet<>(book.getCategories()));
+    }
+
+    @Named("bookFromId")
+    default Book bookFromId(Long id) {
+        //TODO: implement method
+        return null;
+    }
 }
