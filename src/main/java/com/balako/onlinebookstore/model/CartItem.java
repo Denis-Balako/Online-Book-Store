59911b5f18a7d775fd2aa46751dcbe317c87e611
Denis Balako <denisarius2000s@gmail.com>
2023-09-12 14:25:24 +0300

    added cart and cart items models, created liquibase changesets for them

---
+++ src/main/java/com/balako/onlinebookstore/model/CartItem.java
@@ -0,0 +1,38 @@
+package com.balako.onlinebookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Getter
+@Setter
+@Entity
+@SQLDelete(sql = "UPDATE cart_item SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
+@Table(name = "cart_item")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", referencedColumnName = "id", nullable = false)
+    private ShoppingCart shoppingCart;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", referencedColumnName = "id", nullable = false)
+    private Book book;
+    @Column(nullable = false)
+    private int quantity;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+}
---
+++ src/main/java/com/balako/onlinebookstore/model/ShoppingCart.java
@@ -0,0 +1,36 @@
+package com.balako.onlinebookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Getter
+@Setter
+@Entity
+@SQLDelete(sql = "UPDATE cart SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
+@Table(name = "cart")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+    @OneToMany(mappedBy = "shoppingCart")
+    private Set<CartItem> cartItems;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+}
