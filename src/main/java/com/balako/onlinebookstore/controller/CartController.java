d4329fea34ab0fbb79402a5b2a24d410774eebdd
Denis Balako <denisarius2000s@gmail.com>
2023-09-14 13:31:00 +0300

    added swagger support

--- src/main/java/com/balako/onlinebookstore/controller/CartController.java
+++ src/main/java/com/balako/onlinebookstore/controller/CartController.java
@@ -5,10 +5,12 @@ import com.balako.onlinebookstore.dto.cart.request.UpdateCartItemDto;
 import com.balako.onlinebookstore.dto.cart.response.CartDto;
 import com.balako.onlinebookstore.dto.cart.response.CartItemDto;
 import com.balako.onlinebookstore.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,24 +28,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartController {
     private final CartService cartService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
+    @Operation(summary = "Create a new cart item.",
+            description = "Create a new cart item in cart. "
+                    + "Validation included.")
     public CartItemDto create(@RequestBody @Valid CreateCartItemDto requestDto) {
         return cartService.save(requestDto);
     }
 
     @GetMapping
+    @Operation(summary = "Get all cart items from user cart.")
     public CartDto getAll() {
         return cartService.findAll();
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/cart-items/{id}")
+    @Operation(summary = "Update a cart item by ID",
+            description = "Update a cart item by ID. "
+            + "Validation included.")
     public CartItemDto update(@PathVariable Long id,
             @RequestBody @Valid UpdateCartItemDto requestDto) {
         return cartService.update(id, requestDto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/cart-items/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete a cart item by ID")
     public void delete(@PathVariable Long id) {
         cartService.delete(id);
     }
