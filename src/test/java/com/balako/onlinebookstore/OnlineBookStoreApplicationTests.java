4873e9f2e29ed2a6f20a20c739502a10897e992b
Denis Balako <denisarius2000s@gmail.com>
2023-09-19 16:31:06 +0300

    add docker support and fix bugs with registration

--- src/test/java/com/balako/onlinebookstore/OnlineBookStoreApplicationTests.java
+++ src/test/java/com/balako/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -1,13 +1,10 @@
 package com.balako.onlinebookstore;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OnlineBookStoreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
